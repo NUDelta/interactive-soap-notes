eb50ed4bc66e868b520e707d008542af392cd5b6
Kapil Garg <kapil1garg@gmail.com>
2023-03-28 09:51:16 -0500

    SOAP layout with autocomplete

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,24 +4,91 @@ import type { NextPage } from "next";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 
+import TextInput from "react-autocomplete-input";
+import "react-autocomplete-input/dist/bundle.css";
+
 // important that we use dynamic loading here
 // editorjs should only be rendered on the client side.
-const EditorBlock = dynamic(() => import("../components/Editor"), {
-  ssr: false,
-});
+// const EditorBlock = dynamic(() => import("../components/Editor"), {
+//   ssr: false,
+// });
 
 const Home: NextPage = () => {
   //state to hold output data. we'll use this for rendering later
   const [data, setData] = useState<OutputData>();
   return (
-    <div className="grid grid-cols-1 gap-2">
-      <div className="col-span-1">
-        <h1>S - Subjective</h1>
-        <div className="border rounded-md">
-          <EditorBlock
-            data={data}
-            onChange={setData}
-            holder="editorjs-container"
+    // add a header div with the name of the SOAP note
+    // make page centered and 50% width
+    <div className="container m-auto grid grid-cols-2 gap-x-5 gap-y-5 auto-rows-auto w-3/4">
+      <div className="col-span-2 ">
+        <h1 className="font-bold text-4xl">SOAP Notes -- SIG Name, Date</h1>
+      </div>
+
+      {/* Subjective section */}
+      <div className="w-full">
+        <h1 className="font-bold text-xl">
+          S - Subjective observations from student report
+        </h1>
+
+        {/* resizing textbox: https://css-tricks.com/the-cleanest-trick-for-autogrowing-textareas/ */}
+        <div className="">
+          <TextInput
+            trigger={["@", "@@", "[script]"]}
+            options={{
+              "@": ["aa", "ab", "abc", "abcd"],
+              "@@": ["az", "ar"],
+              "[script]": ["script1", "script2"],
+            }}
+          />
+        </div>
+      </div>
+
+      {/* Objective section */}
+      <div className="w-full">
+        <h1 className="font-bold text-xl">
+          O - Objective data corresponding with students assessment
+        </h1>
+
+        <div className="">
+          <TextInput
+            trigger={["@", "@@", "[script]"]}
+            options={{
+              "@": ["aa", "ab", "abc", "abcd"],
+              "@@": ["az", "ar"],
+              "[script]": ["script1", "script2"],
+            }}
+          />
+        </div>
+      </div>
+
+      {/* Assessment section */}
+      <div className="w-full col-span-2">
+        <h1 className="font-bold text-xl">
+          A - Assessment of root causes or practices
+        </h1>
+        <div className="">
+          <TextInput
+            trigger={["@", "@@", "[script]"]}
+            options={{
+              "@": ["aa", "ab", "abc", "abcd"],
+              "@@": ["az", "ar"],
+              "[script]": ["script1", "script2"],
+            }}
+          />
+        </div>
+      </div>
+
+      {/* Plan section */}
+      <div className="w-full col-span-2">
+        <h1 className="font-bold text-xl">P - Plan for Follow-Ups</h1>
+        <div className="">
+          <TextInput
+            trigger={["@", "@@", "[script]"]}
+            options={{
+              "@": ["aa", "ab", "abc", "abcd"],
+              "@@": ["az", "ar"],
+              "[script]": ["script1", "script2"],
+            }}
           />
         </div>
       </div>
