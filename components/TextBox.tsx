c1c6b41a36220c3c0d92a4b762b9813541f66fdf
Kapil Garg <kapil1garg@gmail.com>
2023-04-14 00:38:17 -0500

    hooks and state management for each component of SOAP notes

---
+++ components/TextBox.tsx
@@ -0,0 +1,21 @@
+import TextInput from "react-autocomplete-input";
+import "react-autocomplete-input/dist/bundle.css";
+
+export default function TextBox({
+  value,
+  triggers,
+  options,
+  onChange,
+}): JSX.Element {
+  return (
+    <div className="">
+      <TextInput
+        trigger={triggers}
+        options={options}
+        placeholder="Type here..."
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,9 @@
 import { OutputData } from "@editorjs/editorjs";
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-import TextInput from "react-autocomplete-input";
-import "react-autocomplete-input/dist/bundle.css";
+import TextBox from "../components/TextBox";
 
 // important that we use dynamic loading here
 // editorjs should only be rendered on the client side.
@@ -13,87 +12,125 @@ import "react-autocomplete-input/dist/bundle.css";
 //   ssr: false,
 // });
 
-const Home: NextPage = () => {
-  //state to hold output data. we'll use this for rendering later
-  const [data, setData] = useState<OutputData>();
+export default function Home({
+  data,
+  autocompleteTriggersOptions,
+}): JSX.Element {
+  // hold data from the current soap notes
+  const [soapData, setSoapData] = useState(data);
+
+  // listen for changes in state and do debounced saves to database
+  useEffect(() => {
+    // TODO: debounce the save
+    const timeout = setTimeout(() => {
+      // make request to save the data to the database
+      // TODO: write middleware that converts the raw text into whatever components we need for the backend (e.g., scripts that are triggered; follow-ups that are scheduled)
+      console.log("saving to database", soapData);
+    }, 1000);
+  }, [soapData]);
+
+  // sections of the soap notes
+  const sections = [
+    {
+      name: "subjective",
+      title: "S - Subjective observations from student report",
+    },
+    {
+      name: "objective",
+      title: "O - Objective data corresponding with students assessment",
+    },
+    {
+      name: "assessment",
+      title: "A - Assessment of root causes or practices",
+    },
+    {
+      name: "plan",
+      title: "P - Plan for Follow-Ups",
+    },
+  ];
+
   return (
-    // add a header div with the name of the SOAP note
-    // make page centered and 50% width
-    <div className="container m-auto grid grid-cols-2 gap-x-5 gap-y-5 auto-rows-auto w-3/4">
-      <div className="col-span-2 ">
-        <h1 className="font-bold text-4xl">SOAP Notes -- SIG Name, Date</h1>
-      </div>
+    <>
+      <div className="container m-auto grid grid-cols-2 gap-x-5 gap-y-5 auto-rows-auto w-3/4">
+        <div className="col-span-2 ">
+          <h1 className="font-bold text-4xl">SOAP Notes -- SIG Name, Date</h1>
+          <div></div>
+        </div>
 
-      {/* Subjective section */}
-      <div className="w-full">
-        <h1 className="font-bold text-xl">
-          S - Subjective observations from student report
-        </h1>
+        {/* Section for tracked practices */}
+        <div className="w-full col-span-2">
+          <h1 className="font-bold text-xl">
+            T - Tracked Practices (if applicable)
+          </h1>
+        </div>
 
+        {/* Create a section for each component of the SOAP notes */}
         {/* resizing textbox: https://css-tricks.com/the-cleanest-trick-for-autogrowing-textareas/ */}
-        <div className="">
-          <TextInput
-            trigger={["@", "@@", "[script]"]}
-            options={{
-              "@": ["aa", "ab", "abc", "abcd"],
-              "@@": ["az", "ar"],
-              "[script]": ["script1", "script2"],
-            }}
-          />
-        </div>
-      </div>
+        {sections.map((section) => (
+          <div className="w-full" key={section.name}>
+            <h1 className="font-bold text-xl">{section.title}</h1>
 
-      {/* Objective section */}
-      <div className="w-full">
-        <h1 className="font-bold text-xl">
-          O - Objective data corresponding with students assessment
-        </h1>
-
-        <div className="">
-          <TextInput
-            trigger={["@", "@@", "[script]"]}
-            options={{
-              "@": ["aa", "ab", "abc", "abcd"],
-              "@@": ["az", "ar"],
-              "[script]": ["script1", "script2"],
-            }}
-          />
-        </div>
+            <div className="">
+              <TextBox
+                value={soapData[section.name]}
+                triggers={Object.keys(
+                  autocompleteTriggersOptions[section.name]
+                )}
+                options={autocompleteTriggersOptions[section.name]}
+                onChange={(edits) => {
+                  setSoapData((prevSoapData) => {
+                    let newSoapData = { ...prevSoapData };
+                    newSoapData[section.name] = edits;
+                    return newSoapData;
+                  });
+                }}
+              />
+            </div>
+          </div>
+        ))}
       </div>
-
-      {/* Assessment section */}
-      <div className="w-full col-span-2">
-        <h1 className="font-bold text-xl">
-          A - Assessment of root causes or practices
-        </h1>
-        <div className="">
-          <TextInput
-            trigger={["@", "@@", "[script]"]}
-            options={{
-              "@": ["aa", "ab", "abc", "abcd"],
-              "@@": ["az", "ar"],
-              "[script]": ["script1", "script2"],
-            }}
-          />
-        </div>
-      </div>
-
-      {/* Plan section */}
-      <div className="w-full col-span-2">
-        <h1 className="font-bold text-xl">P - Plan for Follow-Ups</h1>
-        <div className="">
-          <TextInput
-            trigger={["@", "@@", "[script]"]}
-            options={{
-              "@": ["aa", "ab", "abc", "abcd"],
-              "@@": ["az", "ar"],
-              "[script]": ["script1", "script2"],
-            }}
-          />
-        </div>
-      </div>
-    </div>
+    </>
   );
-};
+}
 
-export default Home;
+// use serverside rendering to generate this page
+export const getServerSideProps = async () => {
+  // setup the page with the data from the database
+  // TODO: populate view with the following
+  // - list of tracked practices and when they last occurred
+  // - new work needs to address this week
+  // - Subjective: student self-reflections on how the week went
+  // - Objective: orchestration scripts that monitor for work practices
+  const data = {
+    subjective: "test content for subjective",
+    objective: "test content for objective",
+    assessment: "test content for assessment",
+    plan: "test content for plan",
+  };
+
+  // setup triggers and options for each section's text boxes
+  // TODO: have controllers that abstract this
+  const autocompleteTriggersOptions = {
+    subjective: {},
+    objective: {},
+    assessment: {
+      "#": [
+        "planning/risk-assessment",
+        "planning/stories",
+        "planning/deliverables",
+        "planning/tasks",
+      ],
+    },
+    plan: {
+      "[script]": [
+        " script tempalate for project",
+        " script template for people",
+      ],
+      "[follow-up]": [" follow-up template at next SIG meeting"],
+    },
+  };
+
+  return {
+    props: { data, autocompleteTriggersOptions },
+  };
+};
