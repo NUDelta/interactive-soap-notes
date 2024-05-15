fff885dbc94a680247c1e451d1ce5a94e7fa35c4
Kapil Garg <kapil1garg@gmail.com>
2024-05-15 07:42:03 +0000

    disabling drag for current issues for now

--- components/CurrWeekIssueCard.tsx
+++ components/CurrWeekIssueCard.tsx
@@ -155,7 +155,7 @@ export default function CurrWeekIssueCard({
 
   return (
     <div
-      ref={ref}
+      // ref={ref}
       className={`basis-1/3 shrink-0 p-1 ${selectedIssue === issueId && !isActive ? 'bg-blue-200' : backgroundColor} ${isAddIssue ? 'border-2 border-dashed shadow-none' : 'border-4 shadow hover:border-4 hover:border-blue-300 hover:shadow-none'} ${opacity}`}
       onClick={() => {
         if (!isAddIssue) {
