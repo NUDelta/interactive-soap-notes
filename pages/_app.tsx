44ec48dbc5911bfaae28931342d39fb380fd5964
Kapil Garg <kapil1garg@gmail.com>
2025-10-01 10:43:36 +0000

    updated icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🧼</text></svg>"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='100'>🧢</text></svg>"
         />
       </Head>
 
